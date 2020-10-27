5c093ac83dc7ca716ef0957b2a1ead81c16c0813
Roman Zavodkin <greenfoxer@gmail.com>
2020-10-27 20:43:37 +0300

    Program

---
+++ testJuryNumberProject/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace testJuryNumberProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //// ГОТОВАЯ ПРОГРАММА
+            /*
+            if (args.Length != 5)
+            {
+                Console.WriteLine("Неверное количество параметров. Нужно 6: 1.dot{0,1} 2.количество знаков{1,2,3} 3.delta X {число с точкой} 4.delta Y {число с точкой} 5. delta Z {число с точкой} 6. Путь к файлу");
+                return;
+            }
+            bool isNeedDot = true;
+            if (args[0] == "1")
+                isNeedDot = true;
+            else if (args[0] == "0")
+                isNeedDot = false;
+            else
+            {
+                Console.WriteLine("Неверное значение параметра IsNeedDot");
+            }
+
+            int countAfterDor = int.Parse(args[1]);
+
+            var printer = new NumberPrinter(isNeedDot, countAfterDor);
+
+            double deltaX = double.Parse(args[2], NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture);
+            double deltaY = double.Parse(args[3], NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture);
+            double deltaZ = double.Parse(args[4], NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture);
+
+            string Path = args[5]; // открыть и прочитать файл в FileContent
+            string FileContent = "G0 X500.2365 Y300.12354 Z-50.25648 F2500";
+
+            string result = FileContent;
+
+            result = StrategyForCoordinateStart('X', result, deltaX, printer);
+            result = StrategyForCoordinateStart('Y', result, deltaY, printer);
+            result = StrategyForCoordinateStart('Z', result, deltaZ, printer);
+
+            Console.WriteLine(result);*/
+            ///ТЕСТОВАЯ ПРОГРАММА
+            string FileContent = "G0 X500.2365 Y300.12354 Z-50.25648 F2500";
+            var printer = new NumberPrinter(true, 2);
+            string result = FileContent;
+            result = StrategyForCoordinateStart('X', result, -100, printer);
+            result = StrategyForCoordinateStart('Y', result, 100, printer);
+            result = StrategyForCoordinateStart('Z', result, 0, printer);
+            Console.WriteLine(result);
+        }
+        static string StrategyForCoordinateStart(char cords, string data, double delta, NumberPrinter printer)
+        {
+            var parser = new ValueFinder(cords, delta, data, printer);
+            return parser.Process();
+        }
+    }
+}
---
+++ testJuryNumberProject/ValueFinder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace testJuryNumberProject
+{
+    public class ValueFinder
+    {
+        private char Cords { get; set; }
+        private double Delta { get; set; }
+        private string Data { get; set; }
+        NumberPrinter Printer { get; set; }
+        public ValueFinder(char cordsToFind, double delta, string dataToProcess, NumberPrinter printer)
+        {
+            Cords = cordsToFind;
+            Delta = delta;
+            Data = dataToProcess;
+            Printer = printer;
+        }
+
+        public string Process()
+        {
+            StringBuilder result = new StringBuilder();
+
+            bool stateCollectNumber = false;
+            StringBuilder collectedValue = new StringBuilder();
+
+            foreach (var letter in Data)
+            {
+                if (stateCollectNumber)
+                {
+                    if (!char.IsWhiteSpace(letter))
+                        collectedValue.Append(letter);
+                    else
+                    {
+                        stateCollectNumber = false;
+                        var currentValue = Printer.Print(GetSummaryValue(collectedValue.ToString()));
+                        result.Append(currentValue);
+                        result.Append(letter);
+                    }
+                }
+                else
+                    result.Append(letter);
+
+                if (letter == Cords)
+                    stateCollectNumber = true;
+            }
+
+            return result.ToString();
+        }
+
+        private double GetSummaryValue(string v)
+        {
+            double value = double.Parse(v, NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture);
+            value += Delta;
+            return value;
+        }
+    }
+}
